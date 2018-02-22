f9f10eec95069607f62fcc9b13d67b6ded2550e1
Manisha Narayan <manishan@Wests-MacBook-Pro.local>
2018-02-22 09:53:01 +0530

    Initial Commit

---
+++ TinderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderApp
+//
+//  Created by Manisha Reddy Narayan on 22/02/18.
+//  Copyright © 2018 Manisha Reddy Narayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
